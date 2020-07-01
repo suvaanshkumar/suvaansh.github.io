32f3aaf98c17091aa5a2ba3df2b3a90c1a1d4853
suvaanshkumar <34331549+suvaanshkumar@users.noreply.github.com>
2020-06-30 21:56:47 -0400

    adding social media

--- javascript/main.js
+++ javascript/main.js
@@ -27,9 +27,32 @@ function fill(x,divname){
 function clear(divname){
   document.getElementById(divname).innerHTML="";
 }
+//sticky navbar
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//sticky navbar end
 
 //header external
+$('.ss').click(function(){
+	$('.hero, .content').addClass('scrolled');
+});
 $('.sd').click(function(){
 	$('.hero, .content').addClass('scrolled');
 });
